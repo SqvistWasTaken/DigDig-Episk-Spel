b89f8c9699beb708bb20b25035189ff1c0e66d32
SqvistWasTaken <noa.sjoqvist@icloud.com>
2024-01-25 21:48:40 +0100

    Seperate Tilemaps
    Two seperate tilemaps. Ground tilemap is only cosmetic while collision tilemap contains a tilemap collider component. (Every tile that should be collided with should be in the collision tilemap, any other tiles should be in ground tilemap!)

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f; // Adjust this value to control movement speed
+    private Vector2 pos;
+    [SerializeField] private bool isPlayer1;
+    private float xInput, yInput;
+
+    void Update()
+    {
+        if (isPlayer1)
+        {
+            xInput = Input.GetAxis("Horizontal");
+            yInput = Input.GetAxis("Vertical");
+        }
+        else
+        {
+            xInput = Input.GetAxis("Horizontal2");
+            yInput = Input.GetAxis("Vertical2");
+        }
+
+
+
+        pos += new Vector2(xInput * speed * Time.deltaTime, yInput * speed * Time.deltaTime); // Adds horizontal & vertical movement to pos
+
+        transform.position = pos; // Moves the transform/position to pos
+    }
+}
