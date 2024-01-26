609aed468c7f0a667c162a489a80b8452eddd139
SqvistWasTaken <noa.sjoqvist@icloud.com>
2024-01-26 08:00:23 +0100

    Physics movement
    Changed movement from setting transform to setting velocity. It changes nothing about the movement itself since it sets the velocity and not accelerating. The difference is that it uses collisions. Also fixed the small gap you can see between tiles.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,11 +1,18 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 5f; // Adjust this value to control movement speed
     private Vector2 pos;
+    private Rigidbody2D rb;
     [SerializeField] private bool isPlayer1;
     private float xInput, yInput;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
@@ -19,11 +26,11 @@ public class PlayerMovement : MonoBehaviour
             xInput = Input.GetAxis("Horizontal2");
             yInput = Input.GetAxis("Vertical2");
         }
+        
+        rb.velocity = new Vector2(xInput * speed *100 * Time.deltaTime, yInput * speed * 100 * Time.deltaTime);
 
+        //pos += new Vector2(xInput * speed * Time.deltaTime, yInput * speed * Time.deltaTime); // Adds horizontal & vertical movement to pos
 
-
-        pos += new Vector2(xInput * speed * Time.deltaTime, yInput * speed * Time.deltaTime); // Adds horizontal & vertical movement to pos
-
-        transform.position = pos; // Moves the transform/position to pos
+        //transform.position = pos; // Moves the transform/position to pos
     }
 }
