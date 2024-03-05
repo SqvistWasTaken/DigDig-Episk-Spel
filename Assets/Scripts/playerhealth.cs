e58c226a44ceaeaa5ae89687e4dda2a023099b5b
Reshiram / Alex <145565695+Resh1ram0@users.noreply.github.com>
2024-03-05 01:15:46 +0100

    A half working hp script and a WIP auto aim
    cuhhh

---
+++ Assets/Scripts/playerhealth.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerhealth : MonoBehaviour
+{
+    [SerializeField] public float startingHealth;
+    public float currentHealth;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth == 0)
+        {
+            //player dies (generally in a single hit and gets sent back to main menu scene.
+        }
+    }
+}
