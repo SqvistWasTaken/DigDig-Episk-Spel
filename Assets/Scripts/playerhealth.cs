f96a97f9bab50a17539a22f70183f0ba50b0ef07
Zedex8848 <150916771+Zedex8848@users.noreply.github.com>
2024-03-06 13:07:11 +0100

    codes

--- Assets/Scripts/playerhealth.cs
+++ Assets/Scripts/playerhealth.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerhealth : MonoBehaviour
 {
     [SerializeField] public float startingHealth;
     public float currentHealth;
+    
+    [SerializeField] public float startingHealth2;
+    public float currentHealth2;
     // Start is called before the first frame update
     private void Awake()
     {
         currentHealth = startingHealth;
+        currentHealth2 = startingHealth2;
     }
 
     // Update is called once per frame
@@ -17,9 +22,10 @@ public class playerhealth : MonoBehaviour
     {
         currentHealth = Mathf.Clamp(currentHealth - Takedamage, 0, startingHealth);
 
-        if (currentHealth == 0)
+        if (currentHealth || currentHealth2 == 0)
         {
             //player dies (generally in a single hit and gets sent back to main menu scene.
+            SceneManager.LoadScene("MainMenu");
         }
     }
 }
