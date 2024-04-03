1fafdb7d080cf080a322475238d1d04df72b736e
SqvistWasTaken <noa.sjoqvist@icloud.com>
2024-04-03 13:12:24 +0200

    Death sound effect, improved enemy graphics

--- Assets/Scripts/playerhealth.cs
+++ Assets/Scripts/playerhealth.cs
@@ -8,12 +8,16 @@ public class playerhealth : MonoBehaviour
     [SerializeField] public float maxHealth;
     [HideInInspector] public float health;
 
+    [SerializeField] private AudioClip deathSound;
+    private AudioSource source;
+
     private Animator anim;
 
     private void Start()
     {
         health = maxHealth;
         anim = GetComponentInChildren<Animator>();
+        source = GetComponentInChildren<AudioSource>();
     }
 
     public void TakeDamage(float damageAmount)
@@ -31,6 +35,9 @@ public class playerhealth : MonoBehaviour
             {
                 enemy.CheckPlayers();
             }
+
+            source.pitch = Random.Range(0.8f, 1.2f);
+            source.PlayOneShot(deathSound);
             
             Destroy(gameObject, 2);
         }
